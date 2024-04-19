5f812d29f0cf72e0059bea4037b639af3c2c0307
EdgyGuy19 <edgar.polynskiy.04@gmail.com>
2024-04-20 00:30:33 +0200

    Add animations and player inputs

--- player.gd
+++ player.gd
@@ -2,16 +2,34 @@ extends CharacterBody2D
 
 @export var speed : float = 100
 
-func _physics_process(_delta):
-	# Inputs for directions
-	var input_direction = Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("down") - Input.get_action_strength("up")
-	)
+@onready var ap = $AnimationPlayer
+@onready var sprite = $Sprite2D
 
-	print(input_direction)
+func _physics_process(delta):
+	
+#Getting inputs from the player
+	var horizontal_direction = Input.get_axis("left", "right")
+	var vertical_direction = Input.get_axis("up", "down")
+	
+#Switching direction from left to right
+	if horizontal_direction != 0:
+		sprite.flip_h = Input.get_action_raw_strength("left")
 
-#Update velocity
-	velocity = input_direction * speed
+#Vector directions
+	velocity.x = speed * horizontal_direction
+	velocity.y = speed * vertical_direction
+
+#Printing vector(for testing)
+	print(velocity)
+
 	move_and_slide()
-#Moving
+	
+#Playing the animations
+	animations(horizontal_direction)
+	
+#Function for changing animations
+func animations(direction):
+	if direction == 0:
+		ap.play("idle")
+	else:
+		ap.play("run")
